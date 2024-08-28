e14c299bd5455d181100b48f0b5c99f409f28b11
khalidgomaa <khaled.gomaagedo@gmail.com>
2024-08-28 20:27:09 +0300

    after creating create box

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+
+class TodoList extends Component
+{
+  #[Rule('required|min:3|max:50')]
+  public $name ;
+  public $search;
+
+    public function create()
+    {
+   
+      $validated=$this->validateOnly( 'name');
+      Todo::create($validated);
+      $this->reset('name');
+      session()->flash('success','created succeeefully');
+    }
+
+
+    public function render()
+    {
+   
+        $todos='Todo::paginate(6)';
+
+        return view('livewire.todo-list',compact('todos'));
+    }
+}
